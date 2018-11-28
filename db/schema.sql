27076a6d8e1fe54b0fbc51044acd1ab3a7d260d6
Max Wilets <mwilets@gmail.com>
2018-11-28 14:29:59 -0700

    start up

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE burgers;
+
+CREATE TABLE burgers(
+id INT auto_increment,
+burger_name varchar(250),
+devoured boolean,
+primary key(id)
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers(burger_name, devoured)VALUES
+('rodeo', false),
+('ranch', false),
+('sunrise', false);
