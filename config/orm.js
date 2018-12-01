45f5484b00507e3c0a69c0d2e1e585f987df8c18
Max Wilets <mwilets@gmail.com>
2018-12-01 10:16:50 -0700

    fixedui+

--- config/orm.js
+++ config/orm.js
@@ -1,34 +1,6 @@
 var connection = require('../config/connection')
 
-printQuestionMark = (num) => {
-    var arr = [];
-    for (i = 0; i < num; i++) {
-        arr.push('?')
-    }
-    return arr.toString()
-}
 
-objToSql = (ob) => {
-    var arr = [];
-
-    // loop through the keys and push the key/value as a string int arr
-    for (var key in ob) {
-        var value = ob[key];
-        // check to skip hidden properties
-        if (Object.hasOwnProperty.call(ob, key)) {
-            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
-            if (typeof value === "string" && value.indexOf(" ") >= 0) {
-                value = "'" + value + "'";
-            }
-            // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-            // e.g. {sleepy: true} => ["sleepy=true"]
-            arr.push(key + "=" + value);
-        }
-    }
-
-    // translate array of strings to a single comma-separated string
-    return arr.toString();
-}
 
 orm = {
     selectAll: (table, cb) => {
